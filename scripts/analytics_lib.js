85abd7a1e9aa3e2de91e6d2648cefc02550923f8
David Chanin <dchanin@quizlet.com>
2014-02-22 12:22:08 -0800

    commenting out google analytics code

--- scripts/analytics_lib.js
+++ scripts/analytics_lib.js
@@ -5,14 +5,17 @@
  +-----------------------------------------------------------*/
 
  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-24927696-1']);
+  _gaq.push(['_setAccount', 'UA-xxxxxxxx-x']);
   _gaq.push(['_trackPageview']);
 
+// TODO: set up GA account and uncomment the below lines
+/*
   (function() {
 	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+*/
 
   _trackClickEvent = function (category, action, label) {
 		if (typeof(_gaq) != 'undefined')
